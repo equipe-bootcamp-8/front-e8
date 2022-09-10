6856fc762095d3412914c3edf7e3c9ed741ee102
Bia <biadias20021114@gmail.com>
2022-09-10 13:50:10 -0300

    first navbar -not finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import Navbar from 'components/Navbar';
 import Router from 'router';
 import './App.css';
 
 function App() {
   return (
+    <>
     <Router/>
-  );
+    <Navbar/>
+    </>
+    );
 }
 
 export default App;
