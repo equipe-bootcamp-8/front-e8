ea8b19be9eeb0175ae3787f499a615f321e76608
Bia <biadias20021114@gmail.com>
2022-09-12 21:37:19 -0300

    navbar refactoring

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,43 +1,59 @@
-import * as styled from "./styles";
-import { LogoIcon } from "../../assets/icons/index";
+import { LogoIcon } from "assets/icons";
 import { useState } from "react";
-
+import "./styles.css";
 
 function Navbar() {
-  const [extendNavbar, setExtendNavbar] = useState<boolean>(true);
+  const [active, setActive] = useState("nav__menu");
+  const [icon, setIcon] = useState("nav__toggler");
+  const navToggle = () => {
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+    } else setActive("nav__menu");
 
+
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
+  };
   return (
-    <styled.NavbarContainer extendNavbar={extendNavbar}>
-    <styled.NavbarInnerContainer>
-      <styled.RightContainer>
-        <styled.NavbarLinkContainer>
-          <styled.NavbarLink to="/"> Home</styled.NavbarLink>
-          <styled.NavbarLink to="/products"> Products</styled.NavbarLink>
-          <styled.NavbarLink to="/contact"> Contact Us</styled.NavbarLink>
-          <styled.NavbarLink to="/about"> About Us</styled.NavbarLink>
-          <styled.OpenLinksButton
-            onClick={() => {
-              setExtendNavbar((curr) => !curr);
-            }}
-          >
-            {extendNavbar ? <>&#10005;</> : <> &#8801;</>}
-          </styled.OpenLinksButton>
-        </styled.NavbarLinkContainer>
-      </styled.RightContainer>
-      <styled.LeftContainer>
-        <LogoIcon />
-      </styled.LeftContainer>
-    </styled.NavbarInnerContainer>
-    {extendNavbar && (
-      <styled.NavbarExtendedContainer>
-        <styled.NavbarLinkExtended to="/"> Home</styled.NavbarLinkExtended>
-        <styled.NavbarLinkExtended to="/products"> Products</styled.NavbarLinkExtended>
-        <styled.NavbarLinkExtended to="/login"> Login </styled.NavbarLinkExtended>
-        <styled.NavbarLinkExtended to="/about"> About Us</styled.NavbarLinkExtended>
-      </styled.NavbarExtendedContainer>
-    )}
-  </styled.NavbarContainer>
-);
+    <nav className="nav">
+      <a href="#" className="nav__brand">
+        <LogoIcon/>
+      </a>
+      <ul className={active}>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Home
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            About us
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Products
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Login
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Register
+          </a>
+        </li>
+      </ul>
+      <div onClick={navToggle} className={icon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
