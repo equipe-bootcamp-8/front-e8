8e0daf205e8662e2ec1132ad3491c5f72f25dc9a
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-09-14 19:30:51 -0300

    feat(settings): implement userSettings, 2/3 of dynamic menu

---
+++ src/pages/UserSettings/index.tsx
@@ -0,0 +1,45 @@
+import SettingsMenu from "components/SettingsMenu";
+import * as styled from "./styles";
+
+const UserSettings = () => {
+  return (
+    <styled.SettingsContainer>
+      <SettingsMenu path={"user"} />
+
+      <styled.EditEntityContainer>
+        <h2>Personal informations</h2>
+        <styled.EntityEditList>
+          <styled.ButtonUpload>Upload image</styled.ButtonUpload>
+
+          <styled.PersonalInformations>
+            <div>
+              <h2>E-mail address</h2>
+              <input type="email" />
+            </div>
+
+            <div>
+              <h2>Name</h2>
+              <input type="name" />
+            </div>
+          </styled.PersonalInformations>
+          <styled.PersonalInformations>
+            <div>
+              <h2>Password</h2>
+              <input type="" />
+            </div>
+            <div>
+              <h2>Confirm Password</h2>
+              <input type="" />
+            </div>
+          </styled.PersonalInformations>
+          <styled.Buttons>
+            <styled.SaveButton>Save Changes</styled.SaveButton>
+            <styled.DiscardButton>Discard changes</styled.DiscardButton>
+          </styled.Buttons>
+        </styled.EntityEditList>
+      </styled.EditEntityContainer>
+    </styled.SettingsContainer>
+  );
+};
+
+export default UserSettings;
