1a333027e86090324e4b244fbd0cbe5b5c200ad4
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-09-14 19:29:45 -0300

    feat(settings): implement settingsMenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import theme from 'assets/styles/theme';
 import Navbar from 'components/Navbar';
 import Router from 'router';
+import { ThemeProvider } from 'styled-components';
 import './App.css';
 
 function App() {
   return (
     <>
-    <Router/>
+    <ThemeProvider theme={theme}>
     <Navbar/>
+    <Router/>
+    </ThemeProvider>
     </>
     );
 }
---
+++ src/components/SettingsMenu/index.tsx
@@ -0,0 +1,53 @@
+import * as styled from "./styles";
+import { useNavigate } from "react-router-dom";
+
+interface MenuSettingsProps {
+  path: "products" | "user" | "excel";
+}
+
+const SettingsMenu = ({ path }: MenuSettingsProps) => {
+  const navigate = useNavigate();
+  return (
+    <styled.SettingsNavigationContainer>
+
+      <h2>Settings</h2>
+
+      <h3>Company Name</h3>
+      <h3>company@mail.com</h3>
+      <styled.SettingsNavigationButtonsList>
+        <styled.SettingsNavigationButtonsContainer
+          active={path === "products"}
+          onClick={() => navigate("/settings/products")}
+        >
+          <styled.SettingsNavigationButtonSelected active={path === "products"}>
+            
+            <h2>Products</h2>
+        
+          </styled.SettingsNavigationButtonSelected>
+        </styled.SettingsNavigationButtonsContainer>
+
+        <styled.SettingsNavigationButtonsContainer
+          active={path === "user"}
+          onClick={() => navigate("/settings/user")}
+        >
+          <styled.SettingsNavigationButtonSelected active={path === "user"}>
+            <h2>User</h2>
+          </styled.SettingsNavigationButtonSelected>
+        </styled.SettingsNavigationButtonsContainer>
+
+        <styled.SettingsNavigationButtonsContainer
+         active={path === "excel"}
+         onClick={() => navigate("/settings/excel")}
+         >
+          <styled.SettingsNavigationButtonSelected active={path === "excel"}>
+           
+            <h2>Excel</h2>
+
+          </styled.SettingsNavigationButtonSelected>
+        </styled.SettingsNavigationButtonsContainer> 
+      </styled.SettingsNavigationButtonsList>
+    </styled.SettingsNavigationContainer>
+  );
+};
+
+export default SettingsMenu;
