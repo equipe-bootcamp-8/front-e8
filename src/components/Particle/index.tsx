2f87d772ad97ab5630fdd44982952740895fb7ab
unknown <thuliopv360@gmail.com>
2022-09-17 17:49:28 -0300

    feature: addition of particles in the product

---
+++ src/components/Particle/index.tsx
@@ -0,0 +1,72 @@
+import { useCallback, useMemo } from "react";
+import Particles from "react-tsparticles";
+import { Engine } from "tsparticles-engine";
+import { loadSlim } from "tsparticles-slim";
+import { ParticleConfiguration } from "./styles";
+
+const Particle = () => {
+  const options = useMemo(() => {
+    return {
+      interactivity: {
+        background: {
+          color: {
+            value: "#000",
+          },
+        },
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "attract",
+            move: {
+              enable: true,
+              speed: { min: 1, max: 3 },
+            },
+          },
+        },
+        modes: {
+          push: {
+            quantity: 1,
+          },
+          attract: {
+            radius: 600,
+          },
+        },
+      },
+      IDimension: {
+        height: 0,
+      },
+      particles: {
+        links: {
+          enable: true,
+          distance: 250,
+        },
+        move: {
+          enable: true,
+          speed: { min: 10, max: 11 },
+        },
+        opacity: {
+          value: { min: 1, max: 2 },
+        },
+        size: {
+          value: { min: 2, max: 4 },
+        },
+      },
+    };
+  }, []);
+
+  const particlesInit = useCallback((engine: Engine): Promise<void> => {
+    return loadSlim(engine);
+  }, []);
+
+  return (
+    <ParticleConfiguration>
+      <Particles init={particlesInit} options={options} />
+    </ParticleConfiguration>
+  );
+};
+
+export default Particle;
