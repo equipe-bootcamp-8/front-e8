83ec148f4176e4c12546adb5049884aa096d63cc
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-10-05 20:07:00 -0300

    feat(context): implement ProductContext & providers fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
 import { Toaster } from "react-hot-toast";
-import theme from 'assets/styles/theme';
-
-import Router from 'router';
-import { ThemeProvider } from 'styled-components';
-import './App.css';
-
+import Router from "router";
+import "./App.css";
 
 
 function App() {
   return (
     <>
-      <ThemeProvider theme={theme}>
       <Toaster position="bottom-center" reverseOrder={false} />
-        <Router />
-      </ThemeProvider>
+      <Router />
     </>
   );
 }
 
 export default App;
-
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,5 @@
 import axios from "axios";
 
-export const api = axios.create({ baseURL: "http://localhost:3333" });
+const api = axios.create({ baseURL: "http://localhost:3333" });
+
+export default api;
