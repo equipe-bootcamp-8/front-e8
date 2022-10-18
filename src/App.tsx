60752f1cb019480b338b520d82cdabf0f5032873
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2022-10-17 21:17:59 -0300

    fix: navbar dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "components/Navbar";
 import NavBarLoginRegister from "components/NavBarLoginRegister";
+import { useAuth } from "contexts/auth";
 import { Toaster } from "react-hot-toast";
 import Router from "router";
 import "./App.css";
 
 function App() {
   const token = localStorage.getItem("token");
+  const { logged } = useAuth(); //Se tirar isso aqui a navbar para de ficar dinamica, deixar até achar uma solução melhor
   return (
     <>
       <Toaster position="bottom-center" reverseOrder={false} />
