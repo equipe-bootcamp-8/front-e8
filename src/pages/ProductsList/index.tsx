33f468d7fc3ec0a8aa29446c01df29af88910bf2
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2022-10-19 23:32:38 -0300

    fix(ProductList/index): adjust h1 and h2 texts...
    remove(ProductList/index): product and useState...

--- src/pages/ProductsList/index.tsx
+++ src/pages/ProductsList/index.tsx
@@ -3,18 +3,14 @@ import ProductsCard from "components/ProductsCard";
 import * as Styled from "./styles";
 import meeting from "../../assets/imgs/meeting.png";
 import { useProducts } from "contexts/products";
-import { useState } from "react";
-import { Product } from "types";
-
 
 // interface ProductsListProps {
 //   logged?: boolean | any;
 // }
 
- const ProductsList = () => {
+const ProductsList = () => {
   const { products } = useProducts();
-  const [product, setProduct] = useState<Product | undefined>(undefined);
-  
+
   // logged(false);
 
   return (
@@ -22,11 +18,10 @@ import { Product } from "types";
       <Styled.LaterMenu>
         <img src={meeting} alt="Metting foto" />
         <Styled.LaterMenuLetters>
-          <h1>
-            CloudWalk
-            <hr />
-            E-Commerce Manager
-          </h1>
+          <h1>CloudWalk</h1>
+          <hr />
+          <h2>E-commerce Manager</h2>
+          <br />
           <h4>
             Register new products, create <hr />
             promotions and update information on <hr />a single platform.
@@ -50,8 +45,6 @@ import { Product } from "types";
        />
       )}
       </Styled.ProductList>
-      
-     
     </div>
   );
 };
