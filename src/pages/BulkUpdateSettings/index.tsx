dc2691ecce931b3368252664e60312da2a6ca873
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2022-10-31 16:32:09 -0300

    feature: excel button addd to BulkUpdateSettings

--- src/pages/BulkUpdateSettings/index.tsx
+++ src/pages/BulkUpdateSettings/index.tsx
@@ -1,20 +1,27 @@
 import SettingsMenu from "components/SettingsMenu";
 import * as styled from "./styles";
-
-
+import { FileExcel2 } from "@styled-icons/remix-fill/FileExcel2";
 
 const BulkUpdateSettings = () => {
-
   return (
     <styled.SettingsContainer>
       <SettingsMenu path={"update"} />
-      
+
       <styled.EditEntityContainer>
-      <h2>Spreadsheet massive update</h2>
-      <styled.Bar/>
+        <h2>Spreadsheet massive update</h2>
+        <styled.Bar />
         <styled.EntityEditList>
-        <styled.ButtonUpload>Upload Spreadsheet</styled.ButtonUpload>
-        <styled.ButtonUpload>Download File</styled.ButtonUpload>
+          <styled.Input
+            type="file"
+            id="file"
+            accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+          />
+          <styled.Label htmlFor="file">
+            <FileExcel2 />
+            Choose excel sheet
+          </styled.Label>
+          <styled.ButtonUpload>Upload Spreadsheet</styled.ButtonUpload>
+          <styled.ButtonUpload>Download File</styled.ButtonUpload>
         </styled.EntityEditList>
       </styled.EditEntityContainer>
     </styled.SettingsContainer>
