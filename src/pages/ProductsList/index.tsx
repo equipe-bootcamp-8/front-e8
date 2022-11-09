ab8d26f013393b6e2462d9d1672a6f2b816c5135
Thulio Paiva <thuliopv360@gmail.com>
2022-11-08 21:42:30 -0300

    feat(productList): Product filter creation by price

--- src/pages/ProductsList/index.tsx
+++ src/pages/ProductsList/index.tsx
@@ -5,23 +5,42 @@ import meeting from "../../assets/imgs/meeting.png";
 import { useProducts } from "contexts/products";
 import { useEffect, useState } from "react";
 import { useCategories } from "contexts/categories";
-import { Category } from "types";
+import { Category, Product } from "types";
 import * as gStyled from "../../assets/styles/globalStyles";
 
 const ProductsList = () => {
   const { products } = useProducts();
   const { categories } = useCategories();
   const [search, setSearch] = useState("");
-  const [clicked, setClicked] = useState(false);
+
+  const [minPrice, setMinPrice] = useState(
+    products[0]?.price || ({} as Product)
+  );
+
+  const [maxPrice, setMaxPrice] = useState(
+    products[0]?.price || ({} as Product)
+  );
+
+  const [inputValue, setInputValue] = useState<boolean>(false);
 
   const [selectedCategory, setSelectedCategory] = useState<any>(
     categories[0] || ({} as Category)
   );
 
-  console.log(categories);
+  const [filteredProducts, setFilteredProducts] = useState([{}]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const filterProducts = () => {
+      return products.filter((element) => {
+        if (minPrice <= element.price && element.price <= maxPrice) {
+          return element;
+        }
+      });
+    };
+    setFilteredProducts(filterProducts());
+  }, [maxPrice]);
 
+  console.log(filteredProducts);
   return (
     <div>
       <Styled.LaterMenu>
@@ -38,38 +57,86 @@ const ProductsList = () => {
         </Styled.LaterMenuLetters>
       </Styled.LaterMenu>
       <Styled.FilterContainer>
-        {/* <Styled.Filter>
+        <Styled.Filter>
           <h3>Selected Filters</h3>
-          <p>Preço</p>
-          <input type="range" name="preco" id="preco" />
-          <p>Coleção</p>
+          <p>Price</p>
+          <input
+            type="radio"
+            name="price"
+            id="priceone"
+            onChange={() => {
+              setMinPrice(0);
+              setMaxPrice(350);
+            }}
+          />
+          <label htmlFor="priceone"> Até 350,00</label> <br />
+          <input
+            type="radio"
+            name="price"
+            id="pricetwo"
+            onChange={() => {
+              setMinPrice(351);
+              setMaxPrice(600);
+            }}
+          />
+          <label htmlFor="pricetwo"> 350,00 - 600,00</label> <br />
+          <input
+            type="radio"
+            name="price"
+            id="pricethree"
+            onChange={() => {
+              setMinPrice(601);
+              setMaxPrice(1000);
+            }}
+          />
+          <label htmlFor="pricethree"> 600,00 - 1000,00</label> <br />
+          <input
+            type="radio"
+            name="price"
+            id="pricefour"
+            onChange={() => {
+              setMinPrice(1001);
+              setMaxPrice(9999999);
+            }}
+          />
+          <label htmlFor="pricefour"> Acima de 1000,00</label> <br />
+          <p>Colection</p>
           {categories.length > 0 &&
             categories.map((element) => (
               <Styled.ButtonsContainer>
                 <label htmlFor={element.name}>
-                  <input
-                    type="radio"
-                    placeholder={element.name}
-                    id={element.name}
-                    name="filterCategories"
-                    onChange={() => {
+                  <button
+                    onClick={() => {
                       setSelectedCategory(element);
                     }}
-                  />{" "}
-                  {element.name}
+                  >
+                    {element.name}
+                  </button>
                 </label>
               </Styled.ButtonsContainer>
             ))}
-          <input type="button" value="Filtrar" />
-          <button onClick={() => setSelectedCategory([])}>Clear</button>
-        </Styled.Filter> */}
+          <button
+            onClick={() => {
+              setSelectedCategory([]);
+              setMaxPrice(maxPrice);
+              setMinPrice(0);
+              setInputValue(false);
+            }}
+          >
+            Clear
+          </button>
+        </Styled.Filter>
         <section>
           <Styled.HeaderProductList>
             <gStyled.SearchProduct>
               <div>
                 <SearchIcon />
               </div>
-              <input type="text" placeholder="Search by NFT name..." onChange={(event) => setSearch(event.target.value)} />
+              <input
+                type="text"
+                placeholder="Search by NFT name..."
+                onChange={(event) => setSearch(event.target.value)}
+              />
             </gStyled.SearchProduct>
           </Styled.HeaderProductList>
           <Styled.ProductList>
@@ -79,14 +146,23 @@ const ProductsList = () => {
                   return element;
                 } else return false;
               })
-              // .filter((element) => {
-              //   if (!selectedCategory.name) {
-              //     return element;
-              //   }
-              //   if (selectedCategory.name === element.categoryName) {
-              //     return element;
-              //   }
-              // })
+              .filter((element) => {
+                if (!selectedCategory.name) {
+                  return element;
+                }
+                if (selectedCategory.name === element.categoryName) {
+                  return element;
+                }
+              })
+              .filter((element) => {
+                if (minPrice <= element.price && element.price <= maxPrice) {
+                  return element;
+                }
+                // if (maxPrice) {
+                //   return element;
+                // }
+              })
+
               .map((element) => (
                 <ProductsCard product={element} key={element.id} />
               ))}
