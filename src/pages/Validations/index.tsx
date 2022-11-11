56491cc3897e0716cf193295ab9b7668ff0eb672
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2022-11-11 18:57:13 -0300

    feat(Validation): patch user.active

--- src/pages/Validations/index.tsx
+++ src/pages/Validations/index.tsx
@@ -1,12 +1,15 @@
 import { useAuth } from "contexts/auth";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import api from "services";
 import SendEmailVerification from "services/email";
 import * as Styled from "./styles";
 
-const urlParams = new URLSearchParams(window.location.search);
-const idUrl = urlParams.get("id");
-
 const RegisterValidation = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const idUrl = urlParams.get("id");
   const { user } = useAuth();
+  const navigate = useNavigate();
 
   const userData = {
     name: user.name,
@@ -14,7 +17,30 @@ const RegisterValidation = () => {
     email: user.email,
   };
 
-  console.log(user);
+  const confirmEmail = () => {
+    if (idUrl === user.id) {
+      const token = localStorage.getItem("token");
+
+      const headers = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const data = {
+        active: true,
+      };
+
+      api
+        .patch(`/users/${user.id}`, data, headers)
+        .then(() => {
+          toast.success("Confirmed email!");
+          navigate("/settings");
+        })
+        .catch(() => toast.error("Invalid Link!"));
+    }
+  };
+
   return (
     <div>
       <Styled.Background>
@@ -27,10 +53,8 @@ const RegisterValidation = () => {
               <Styled.Copy>
                 <p>We send you a confirmation email.</p>
                 <p>Please confirm your email to complete account activation.</p>
-                <Styled.SendEmailButton
-                  onClick={() => SendEmailVerification(userData)}
-                >
-                  Resend confirmation email
+                <Styled.SendEmailButton onClick={() => confirmEmail()}>
+                  Confirm email!
                 </Styled.SendEmailButton>
               </Styled.Copy>
             </Styled.Container>
