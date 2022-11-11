b6c002dd25a96b9664048926f1b1cb97e674387e
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-11-11 00:11:08 -0300

    bg fix

--- src/pages/ProductSettings/styles.ts
+++ src/pages/ProductSettings/styles.ts
@@ -4,19 +4,6 @@ interface ActiveButtonProps {
   active?: boolean;
 }
 
-
-export const SettingsContainer = styled.div`
-  ${({ theme }) => css`
-    background-image: url(${backgroundProduct});
-    min-width: 90vw;
-    height: calc(100vh - 10vh);
-    color: ${theme.colors.textColor};
-    display: flex;
-    overflow-y: hidden;
-  `}
-`;
-
-
 export const Bar = styled.div`
   ${() => css`
     width: 100%;
@@ -28,7 +15,6 @@ export const Bar = styled.div`
 
 export const Header = styled.div`
   ${() => css`
-
   display: flex;
   justify-content: space-between;
   align-items: center;
