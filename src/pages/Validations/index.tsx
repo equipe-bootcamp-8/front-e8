4b4ab730065ba116a5f81875f9136e424bc75ada
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2022-11-11 18:09:13 -0300

    refactor(Validations): logical to idUrl

--- src/pages/Validations/index.tsx
+++ src/pages/Validations/index.tsx
@@ -3,34 +3,56 @@ import SendEmailVerification from "services/email";
 import * as Styled from "./styles";
 
 const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get("id");
-
+const idUrl = urlParams.get("id");
 
 const RegisterValidation = () => {
   const { user } = useAuth();
-  
+
   const userData = {
     name: user.name,
     id: user.id,
     email: user.email,
-};
+  };
 
   console.log(user);
   return (
     <div>
       <Styled.Background>
-        <Styled.Container>
-          <Styled.FormHeader>
-            <h1>Thanks for signing up!</h1>
-          </Styled.FormHeader>
-          <Styled.Copy>
-            <p>We send you a confirmation email.</p>
-            <p>Please confirm your email to complete account activation.</p>
-            <Styled.SendEmailButton onClick={() => (SendEmailVerification(userData))}>
-              Resend confirmation email
-            </Styled.SendEmailButton>
-          </Styled.Copy>
-        </Styled.Container>
+        {idUrl ? (
+          <>
+            <Styled.Container>
+              <Styled.FormHeader>
+                <h1>Clique no Ok pra validar email</h1>
+              </Styled.FormHeader>
+              <Styled.Copy>
+                <p>We send you a confirmation email.</p>
+                <p>Please confirm your email to complete account activation.</p>
+                <Styled.SendEmailButton
+                  onClick={() => SendEmailVerification(userData)}
+                >
+                  Resend confirmation email
+                </Styled.SendEmailButton>
+              </Styled.Copy>
+            </Styled.Container>
+          </>
+        ) : (
+          <>
+            <Styled.Container>
+              <Styled.FormHeader>
+                <h1>Thanks for signing up!</h1>
+              </Styled.FormHeader>
+              <Styled.Copy>
+                <p>We send you a confirmation email.</p>
+                <p>Please confirm your email to complete account activation.</p>
+                <Styled.SendEmailButton
+                  onClick={() => SendEmailVerification(userData)}
+                >
+                  Resend confirmation email
+                </Styled.SendEmailButton>
+              </Styled.Copy>
+            </Styled.Container>
+          </>
+        )}
       </Styled.Background>
     </div>
   );
